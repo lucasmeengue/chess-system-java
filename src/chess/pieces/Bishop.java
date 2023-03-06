84c243ac1e1a8269d248039cb94f555e144fdd8a
Lucas Mengue <lucaskmengue@gmail.com>
2023-03-06 18:17:19 -0300

    Bishop

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,15 +5,15 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Bishop extends ChessPiece {
 
-	public Rook(Board board, Color color) {
+	public Bishop(Board board, Color color) {
 		super(board, color);
 	}
 
 	@Override
 	public String toString() {
-		return "R";
+		return "B";
 	}
 
 	@Override
@@ -21,41 +21,42 @@ public class Rook extends ChessPiece {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0, 0);
 
-		// ABOVE //
-		p.setValues(position.getRow() - 1, position.getColumn());
+		// NW //
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() - 1);
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponetPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// LEFT //
-		p.setValues(position.getRow(), position.getColumn() - 1);
+		// NE //
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() - 1);			
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+			
 		}
 		if (getBoard().positionExists(p) && isThereOpponetPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// RIGHT //
-		p.setValues(position.getRow(), position.getColumn() + 1);
+		// SE //
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setColumn(p.getColumn() + 1);
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponetPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		// BELOW //
-		p.setValues(position.getRow() + 1, position.getColumn());
+		// SW //
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow() + 1);
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponetPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
